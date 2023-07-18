b049c05ad06281f7a778e0d07b34ea7b284254bf
Haeun Kim <kimha0094@naver.com>
2023-07-18 19:22:02 +0900

    jfjf

--- ttttttt/src/ttttttt/ttttt.java
+++ ttttttt/src/ttttttt/ttttt.java
@@ -10,6 +10,8 @@ public class ttttt {
 		System.out.println("tttt");
 		System.out.println("tttt");
 		System.out.println("tttt");
+		System.out.println("tttt");
+		System.out.println("tttt");
 
 	}
 
