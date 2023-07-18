049a81d26d1a55fa4fa0ae1fd5dee11865af4152
Haeun Kim <kimha0094@naver.com>
2023-07-18 19:18:17 +0900

    ddddddd

---
+++ ttttttt/src/ttttttt/ttttt.java
@@ -0,0 +1,13 @@
+package ttttttt;
+
+public class ttttt {
+
+	public static void main(String[] args) {
+		System.out.println("tttt");
+		System.out.println("tttt");
+		System.out.println("tttt");
+		System.out.println("tttt");
+
+	}
+
+}
